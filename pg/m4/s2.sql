47845f212dc721942ab05f4e273b2f8161a650ee
Emanuele <egalli64@gmail.com>
2022-09-17 09:38:59 +0200

    minor review

--- pg/m4/s2.sql
+++ pg/m4/s2.sql
@@ -1,18 +1,29 @@
 -- examples on aggregate functions
 
 -- count, count distinct
-select count(*), count(manager_id), count(distinct manager_id)
+select count(*) as "All employees", count(manager_id) as "Having a manager", 
+	count(distinct manager_id) as "How many managers"
 from employee;
 
-select count(commission), count(distinct commission)
+select count(commission) as "Employees with commission",
+	count(distinct commission) as "How many different commissions"
 from employee;
 
 -- max, min
-select max(salary), min(salary)
+select max(salary) as "Max salary", min(salary) as "Min salary",
+	round(avg(salary)) as "Average salary"
 from employee;
 
-select max(commission), min(distinct commission)
+select max(hired) "Most recent hire" , min(hired) "Earliest hire"
 from employee;
+
+-- spoiler: subquery to get extra info
+select first_name, last_name, salary 
+from employee
+where salary = (
+	select min(salary)
+	from employee
+);
 
 -- min, max on dates
 select min(hired), max(hired)
@@ -23,6 +34,9 @@ select min(first_name), max(last_name)
 from employee;
 
 -- sum
+select sum(salary)
+from employee;
+
 select sum(salary)
 from employee
 where department_id = 5;
--- pg/m4/s3.sql
+++ pg/m4/s3.sql
@@ -6,6 +6,11 @@ from employee
 group by department_id, job_id
 order by department_id;
 
+-- compare group by to select distinct
+select distinct department_id, job_id, first_name, last_name 
+from employee
+order by department_id;
+
 -- average salary for all the employees
 select round(avg(salary), 2) as avg_sal
 from employee;
@@ -16,6 +21,19 @@ from employee
 group by department_id
 order by avg_sal desc;
 
+-- max salary for employees by department, from top downward
+select department_id, max(salary) as sum_salary
+from employee
+group by department_id
+order by sum_salary desc;
+
+-- same, but showing department name in result set
+select d.name, max(e.salary) as sum_salary
+from employee e join department d
+using (department_id)
+group by d.name
+order by sum_salary desc;
+
 -- average salary for each department, excluding the employees not assigned to a department
 select department_id, round(avg(salary)) as avg_sal
 from employee
@@ -23,7 +41,7 @@ where department_id is not null
 group by department_id
 order by avg_sal desc;
 
--- average salary for latest hired employees grouped by department
+-- average salary for employees hired from 2020 on, grouped by department
 select d.name, round(avg(e.salary)) as avg_sal
 from employee e join department d using (department_id)
 where hired >= '2020-01-01'
