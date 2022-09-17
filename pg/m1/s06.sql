8d9a1063f5ab0539affc5a73c9e2922d4496c2d3
Emanuele <egalli64@gmail.com>
2022-09-17 08:19:12 +0200

    minor review

--- pg/m1/s06.sql
+++ pg/m1/s06.sql
@@ -1,27 +1,27 @@
 -- examples on null
 
 -- check null
-select employee_id, first_name, last_name, commission
+select employee_id, first_name, last_name, commission 
 from employee
 where commission is null
-and last_name = 'King';
+	and last_name = 'King';
 
 select employee_id, first_name, last_name, commission
 from employee
-where commission is not null
-and last_name = 'King';
+where commission is not null 
+	and last_name = 'King';
 
 -- !!! WRONG !!! check null
 select employee_id, first_name, last_name, commission
 from employee
 where commission = null
-and last_name = 'King';
+	and last_name = 'King';
 
 -- !!! WRONG !!!
 select employee_id, first_name, last_name, commission
 from employee
 where commission != null
-and last_name = 'King';
+	and last_name = 'King';
 
 -- null in operations
 select first_name, last_name, salary * commission
@@ -29,6 +29,6 @@ from employee
 where last_name = 'King';
 
 -- coalesce: if commission is null use 0 instead
-select employee_id, last_name, salary * coalesce(commission, 0)
+select employee_id, first_name, last_name, coalesce(commission, 0)
 from employee
 where last_name = 'King';
--- pg/m1/s09.sql
+++ pg/m1/s09.sql
@@ -7,9 +7,9 @@ order by last_name, first_name;
 -- sorting in natural order
 select *
 from employee
-order by salary;
+order by salary asc;
 
--- descending
+-- descending, limited
 select first_name, last_name, salary
 from employee
 order by salary desc
@@ -22,10 +22,11 @@ where first_name like 'A%'
 order by 3 desc
 limit 5;
 
+-- explicit column in order by, "asc" is rarely used
 select first_name, last_name, hired
 from employee
 where first_name like 'A%'
-order by 3 desc
+order by hired asc
 limit 5;
 
 -- column alias in order by 
